3b78c198eb13e73c96e3211ab497d99c0e118fa1
naomu <muramatsu2023@gmail.com>
2024-02-21 00:35:07 +0900

    js.erb編集後

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    #@favorite.save
+    #redirect_to blogs_path
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    #@favorite.save
+    #redirect_to blogs_path
   end
 
 end
